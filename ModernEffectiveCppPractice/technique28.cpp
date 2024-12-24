8cab61b03797759c84d47b045c0e63181ed7ed75
Mingyu0626 <mg010626@naver.com>
2024-12-24 20:07:24 +0900

    Add | 항목 28 및 29 추가

--- ModernEffectiveCppPractice/technique28.cpp
+++ ModernEffectiveCppPractice/technique28.cpp
@@ -12,15 +12,40 @@
 
 using namespace std;
 
+template<typename T>
+void func(T&& param);
+
+template<typename T>
+T&& forward(remove_reference_t<T>& param)
+{
+	return static_cast<T&&>(param);
+}
+
 class Widget {};
 
-
-
+template<typename T>
+class WidgetTemplate 
+{
+public:
+	typedef T&& RvalueRefToT;
+	
+};
 
 
 int main()
 {
+	Widget widgetFactory();   // rvalue를 돌려주는 함수
+	Widget w;
+	func(w);
+	func(widgetFactory());
 
+	auto&& w1 = w; // w1은 lvalue 참조
+	auto&& w2 = widgetFactory(); // w2는 rvalue 참조
+
+
+
+	WidgetTemplate<int&> w;   // int&로 선언
+	typedef int& RvalueRefToT; // lvalue에 대한 typedef가 됨.
 
 	return 0;
 }
