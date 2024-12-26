eecac9e2dd126c1d4647d8021a93cc41276520ee
Mingyu Choi <mg010626@naver.com>
2024-12-26 16:38:56 +0900

    Add | 항목 30 및 31 추가

--- ModernEffectiveCppPractice/technique30.cpp
+++ ModernEffectiveCppPractice/technique30.cpp
@@ -12,15 +12,102 @@
 
 using namespace std;
 
-class Widget {};
+void f(const vector<int>& v);
+void f(size_t val);
+void f(int (*pf)(int));
+void f(int pf(int));
+void f(std::size_t sz);
+
+template<typename T>
+void fwd(T&& param)
+{
+	f(std::forward<T>(param));
+}
+template<typename... Ts>
+void fwd(Ts&&... params)
+{
+	f(std::forward<Ts>(params)...);
+}
 
 
 
+void f(const vector<int>& v) {}
+void f(size_t val) {}
+
+
+
+class Widget
+{
+public:
+
+	static constexpr size_t MinVals = 28; // MinVals의 선언
+};
+constexpr std::size_t Widget::MinVals; // MinVals의 정의
+
+
+
+void f(int (*pf)(int)) {}
+void f(int pf(int)) {}
+
+int processVal(int value);
+int processVal(int value, int priority);
+
+template<typename T>
+T workOnVal(T param)
+{
+	...
+}
+
+// 전달하고자 하는 오버로딩 or 템플릿 인스턴스를 명시적으로 지정
+using ProcessFuncType = int (*)(int);
+
+
+
+struct IPv4Header {
+	std::uint32_t version : 4,
+		IHL : 4,
+		DSCP : 6,
+		ECN : 2,
+		totalLength : 16;
+};
+
+void f(std::size_t sz) {}
 
 
 int main()
 {
+	// f({ 1, 2, 3 }); // {1, 2, 3}은 암묵적으로 vector<int>로 변환
+	// fwd({ 1, 2, 3 }); // Error!
 
+
+
+	//vector<int> widgetData;
+	//widgetData.reserve(Widget::MinVals); // MinVals를 사용
+
+
+
+	f(Widget::MinVals);     // OK
+	fwd(Widget::MinVals);   // 링크 성공 in VS 2022, But 다른 컴파일러에서는 실패할 가능성 농후
+
+
+
+	f(processVal); // OK!
+
+	fwd(processVal); // Error!
+	fwd(workOnVal);    // Error!
+
+	ProcessFuncType processValPtr = processVal;
+	fwd(processValPtr);
+	fwd(static_cast<ProcessFuncType>(workOnVal));
+
+
+
+	IPv4Header h;
+	f(h.totalLength);   // OK
+	fwd(h.totalLength); // Error!
+
+	auto length = static_cast<std::uint16_t>(h.totalLength);
+	fwd(length); // OK
 
 	return 0;
 }
--- ModernEffectiveCppPractice/technique31.cpp
+++ ModernEffectiveCppPractice/technique31.cpp
@@ -12,11 +12,141 @@
 
 using namespace std;
 
-class Widget {};
+class Widget 
+{
+public:
+	void addFilter() const;
+
+private:
+	int divisor;
+};
+
+using FilterContainer =
+vector<function<bool(int)>>;
+
+FilterContainer filters;
+
+//void addDivisorFilter()
+//{
+//	auto calc1 = computeSomeValue1();
+//	auto calc2 = computeSomeValue2();
+//
+//	auto divisor = computeDivisor(calc1, calc2);
+//
+//	filters.emplace_back(
+//		[&](int value) { return value % divisor == 0; } // 위험하다.
+//	);
+//}
+
+// 기본 참조 캡쳐 모드 사용
+template<typename C>
+void addDivisorFilter(const C& container)
+{
+	auto calc1 = computeSomeValue1();
+	auto calc2 = computeSomeValue2();
+
+	auto divisor = computeDivisor(calc1, calc2);
+
+	using ContElemT = typename C::value_type; // 컨테이너에 담긴 요소들의 형식
+	using std::begin;
+	using std::end; // 일반성을 위한 using 사용, 항목13 참고
+
+	if (std::all_of(
+		begin(container), end(container),
+		[&](const ContElemT& value)
+		{ return value % divisor == 0; }) // 컨테이너의 모든 값이 divisor의 배수인가?
+		)
+	{
+		// 그런 경우
+	}
+	else
+	{
+		// 아닌 경우
+	}
+}
 
 
 
+// 기본 값 캡쳐 모드 사용
+void addDivisorFilter()
+{
+	auto calc1 = computeSomeValue1();
+	auto calc2 = computeSomeValue2();
 
+	auto divisor = computeDivisor(calc1, calc2);
+
+	filters.emplace_back(
+		[=](int value) { return value % divisor == 0; } // 이제는 divisor가 대상을 잃지 않는다.
+	);
+}
+
+
+
+// 기본 캡쳐 모드를 뜻하는 [=]의 = 제거
+void Widget::addFilter() const
+{
+	filters.emplace_back(
+		[](int value) { return value % divisor == 0; } // Error!
+	);
+}
+
+// divisor를 명시적인 값으로 캡쳐
+void Widget::addFilter() const
+{
+	filters.emplace_back(
+		[divisor](int value)                           // Error!
+		{ return value % divisor == 0; }
+	);
+}
+
+// divisor를 지역 변수로 복사하여 해결
+// C++11
+void Widget::addFilter() const
+{
+	auto divisorCopy = divisor; // 자료 멤버를 복사
+	filters.emplace_back(
+		[divisorCopy](int value) // 복사본을 캡쳐
+		{ return value % divisorCopy == 0; } // OK!
+	);
+}
+
+// 기본 캡쳐 모드 사용, But 굳이 위험을 감수할 필요는 없음. 그냥 위에처럼 명시해주자.
+void Widget::addFilter() const
+{
+	auto divisorCopy = divisor; // 자료 멤버를 복사
+	filters.emplace_back(
+		[=](int value) // 복사본을 캡쳐
+		{ return value % divisorCopy == 0; } // OK!
+	);
+}
+
+// C++14
+void Widget::addFilter() const
+{
+	filters.emplace_back(
+		[divisor = divisor](int value) // divisor를 클로저에 복사
+		{ return value % divisor == 0; } // OK!
+	);
+}
+
+
+
+// static 변수를 사용한 예시
+void addDivisorFilter()
+{
+	static auto calc1 = computeSomeValue1();
+	static auto calc2 = computeSomeValue2();
+
+	static auto divisor =
+		computeDivisor(calc1, calc2);
+
+	filters.emplace_back(
+		[=](int value) // 아무 것도 캡쳐하지 않는다.
+		{ return value % divisor == 0; } // 위 정적 변수 divisor를 지칭한다.
+	);
+
+	++divisor; // divisor를 수정한다.
+}
 
 int main()
 {
