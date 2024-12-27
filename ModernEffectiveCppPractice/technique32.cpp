82735926571cdcbe88f0415aeaada79574b1e440
Mingyu0626 <mg010626@naver.com>
2024-12-27 13:47:02 +0900

    Add | 항목 32 및 33 추가

--- ModernEffectiveCppPractice/technique32.cpp
+++ ModernEffectiveCppPractice/technique32.cpp
@@ -12,14 +12,77 @@
 
 using namespace std;
 
-class Widget {};
+class Widget 
+{
+public:
+	bool isValidated() const;
+	bool isProcessed() const;
+	bool isArchived() const;
+
+private:
+	
+};
+
+// C++14
+auto pw = make_unique<Widget>();
+// 클로저의 자료 멤버를 std::move(pw)로 초기화한다.
+auto func = [pw = move(pw)]
+	{ return pw->isValidated() && pw->isArchived(); };
+
+auto func = [pw = std::make_unique<Widget>()]
+	{ return pw->isValidated()
+	&& pw->isArchived(); };
 
 
 
+// C++11
+class IsValAndArch // 유효성 및 보관 여부를 판정하는 클래스
+{
+public:
+	using DataType = unique_ptr<Widget>;
 
+	explicit IsValAndArch(DataType&& ptr)
+		: pw(move(ptr)) {}
+
+	bool operator()() const
+	{
+		return pw->isValidated() && pw->isArchived();
+	}
+
+private:
+	DataType pw;
+};
+auto func = IsValAndArch(make_unique<Widget>());
+
+
+
+// C++14
+auto func = [pw = std::make_unique<Widget>()]
+	{ return pw->isValidated() && pw->isArchived(); };
+
+// C++11
+auto func = bind(
+	[](const unique_ptr<Widget>& pw)
+	{ return pw->isValidated() && pw->isArchived(); },
+	make_unique<Widget>()
+);
 
 int main()
 {
+	vector<double> data;
+	// C++14
+	auto func = [data = std::move(data)]
+		{};
+
+	// C++11
+	auto func =
+		std::bind(
+			[](const std::vector<double>& data)
+			{},
+			std::move(data)
+		);
+
+
 
 
 	return 0;
--- ModernEffectiveCppPractice/technique33.cpp
+++ ModernEffectiveCppPractice/technique33.cpp
@@ -14,13 +14,27 @@ using namespace std;
 
 class Widget {};
 
+auto f = [](auto x) { return normalize(x); };
 
+class 컴파일러가_생성한_클래스
+{
+public:
+	template<typename T>
+	auto operator()(T x) const
+	{
+		return normalize(x);
+	}
+};
 
 
 
 int main()
 {
+	auto f = [](auto&& x)
+		{ return normalize(std::forward<decltype(x)>(x)); };
 
+	auto f2 = [](auto&&... xs)
+		{ return normalize(std::forward<decltype(xs)>(xs)...); };
 
 	return 0;
 }
