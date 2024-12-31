77b9c3e9f5963fc7ad226de45a5b6fa3a7db4bdb
Mingyu Choi <mg010626@naver.com>
2024-12-31 15:28:12 +0900

    Add | 항목 36 및 37 추가

--- ModernEffectiveCppPractice/technique36.cpp
+++ ModernEffectiveCppPractice/technique36.cpp
@@ -5,22 +5,70 @@
 #include <string>
 #include <thread>
 #include <mutex>
+#include <future>
 #include <cassert>
 #include <functional>
 #include <new>
 #include <utility>
 
 using namespace std;
+using namespace std::literals;
 
 class Widget {};
 
-
-
-
+void f() // f는 1초간 수면 후 반환
+{
+	std::this_thread::sleep_for(1s);
+}
 
 int main()
 {
+	auto fut = std::async(f); // 개념적으로는, f를 비동기적으로실행
+	while (fut.wait_for(100ms) != std::future_status::ready)
+	{
+		// f의 실행이 끝날 때까지 루프 반복, 하지만 실행이 끝나지 않을 수도 있다.
+	}
 
+
+
+	auto fut = std::async(f);
+	if (fut.wait_for(0s) == std::future_status::deferred) // 만약 과제가 지연되었다면
+	{
+		// fut에 wait나 get을 호출해서 f를 동기적으로 호출
+	}
+	else // 과제가 지연되지 않았다면
+	{
+		while (fut.wait_for(100ms) != std::future_status::ready)
+			// 무한 루프는 불가능(f가 완료된다고 가정할 때)
+		{
+			// 과제가 지연되지도 않았고, 준비도 되지 않았으므로 준비될 때까지 동시적 작업 수행
+		}
+		// fut가 준비되었다.
+	}
+
+
+
+	// std::launch::async를 이용한 과제를 비동기적으로 실행하도록 강제
+	auto fut = std::async(std::launch::async, f);
 
 	return 0;
+}
+
+// C++11
+template<typename F, typename... Ts>
+inline std::future<typename std::result_of<F(Ts...)>::type>
+reallyAsync(F&& f, Ts&&... params)
+{
+	return std::async(std::launch::async,
+		std::forward<F>(f),
+		std::forward<Ts>(params)...);
+}
+
+// C++14
+template<typename F, typename... Ts>
+inline auto reallyAsync(F&& f, Ts&&... params)
+{
+	return std::async(std::launch::async,
+		std::forward<F>(f),
+		std::forward<Ts>(params)...);
 }
--- ModernEffectiveCppPractice/technique37.cpp
+++ ModernEffectiveCppPractice/technique37.cpp
@@ -14,8 +14,97 @@ using namespace std;
 
 class Widget {};
 
+constexpr auto tenMillion = 10'000'000;
+
+bool doWork(std::function<bool(int)> filter, int maxVal = tenMillion)
+{
+	vector<int> goodVals; // 필터를 통과한 값들
+
+	thread t([&filter, maxVal, &goodVals] // goodVals에 값들을 채운다.
+		{
+			for (auto i = 0; i <= maxVal; i++)
+			{
+				if (filter(i))
+				{
+					goodVals.push_back(i);
+				}
+			}
+		});
+
+	auto nh = t.native_handle(); // t의네이티브 핸들을 이용하여, t의 우선 순위를 설정
+
+	if (conditionsAreSatisfied()) // 조건들이 만족되었다면, t의 완료를 기다린다
+	{
+		t.join();
+		performComputation(goodVals); 
+		return true; // 계산이 수행되면, true 리턴
+	}
+	return false; // 계산이 수행되지 않으면, false 리턴
+}
 
 
+
+class ThreadRAII
+{
+public:
+	enum class DtorAction { join, detach };
+
+	ThreadRAII(std::thread&& t, DtorAction a) // 소멸자에서, t에 대해 동작 a를 수행
+		: action(a), t(std::move(t)) {}
+
+	~ThreadRAII() 
+	{
+		if (t.joinable())
+		{
+			if (action == DtorAction::join)
+			{
+				t.join();
+			}
+			else
+			{
+				t.detach();
+			}
+		}
+	}
+
+	ThreadRAII(ThreadRAII&&) = default;
+	ThreadRAII& operator=(ThreadRAII&&) = default;
+
+	std::thread& get() { return t; }
+
+private:
+	DtorAction action;
+	std::thread t;
+};
+
+
+
+// ThreadRAII를 적용한 doWork()
+bool doWork(std::function<bool(int)> filter, int maxVal = tenMillion)
+{
+	vector<int> goodVals; // 필터를 통과한 값들
+
+	ThreadRAII t(
+		std::thread([&filter, maxVal, &goodVals] // goodVals에 값들을 채운다.
+			{
+				for (auto i = 0; i <= maxVal; i++)
+				{
+					if (filter(i)) goodVals.push_back(i);
+				}
+			}), 
+		ThreadRAII::DtorAction::join
+	);
+
+	auto nh = t.get().native_handle(); // t의네이티브 핸들을 이용하여, t의 우선 순위를 설정
+
+	if (conditionsAreSatisfied()) // 조건들이 만족되었다면, t의 완료를 기다린다
+	{
+		t.get().join();
+		performComputation(goodVals);
+		return true; // 계산이 수행되면, true 리턴
+	}
+	return false; // 계산이 수행되지 않으면, false 리턴
+}
 
 
 int main()
